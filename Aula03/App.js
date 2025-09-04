f65a0d433124c460364cc31fd682bc327cbb4293
Mateus Moreira <mateusmoura023@gmail.com>
2025-09-04 23:32:42 +0000

    (28/08) Atividade Aula 03

--- Aula03/App.js
+++ Aula03/App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Configuração do ambiente de desenvolvimento - Aula 03</Text>
+      <Text style={styles.titulo}>Projeto Aula03 Personalizado!</Text>
+      <Text style={styles.subtitulo}>Alterando texto e estilos conforme solicitado.</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#e0f7fa' },
+  titulo: { fontSize: 28, fontWeight: 'bold', color: '#00796b' },
+  subtitulo: { fontSize: 18, color: '#004d40', marginTop: 10 }
 });
+
+// npx expo start --web
